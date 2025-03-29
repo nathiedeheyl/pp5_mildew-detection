7269573e51a9fbb7f4a019383a88811778359823
nathiedeheyl <n.vonheyl@outlook.com>
2025-03-29 21:19:40 +0100

    refactor: update code syntax, mainly identation, and shorten line lenght

--- src/page_manager.py
+++ src/page_manager.py
@@ -1,15 +1,17 @@
 import streamlit as st
 
+
 # Manage multiple Streamlit pages in one app
 class MultiPage:
-    
+
     def __init__(self, app_name):
         self.app_name = app_name
         self.pages = []
 
         st.set_page_config(
-        page_title="Cherry Leaf Powdery Mildew Detector",
-        page_icon="🍒",)
+          page_title="Cherry Leaf Powdery Mildew Detector",
+          page_icon="🍒",
+        )
 
     def add_page(self, title, func):
         """
@@ -21,7 +23,14 @@ class MultiPage:
         """
         Run the multipage app
         """
-        st.title("Cherry Leaf Powdery Mildew Detection")
-        st.write("This application uses a CNN model to detect powdery mildew in cherry leaves.")
-        page = st.sidebar.radio('Page Navigation', self.pages, format_func=lambda page: page["title"])
+        st.title("Cherry Leaf Powdery Mildew Detector")
+        st.write(
+            "Use the sidebar to navigate through the project"
+            "details and tools."
+            )
+        page = st.sidebar.radio(
+            'Page Navigation',
+            self.pages,
+            format_func=lambda page: page["title"]
+            )
         page["function"]()
