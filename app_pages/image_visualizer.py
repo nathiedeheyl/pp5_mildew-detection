48c2e165e63ea92175614a42f12144c0744b112b
nathiedeheyl <n.vonheyl@outlook.com>
2025-03-29 22:02:42 +0100

    refactor: update mainly indentantion and line length

--- app_pages/image_visualizer.py
+++ app_pages/image_visualizer.py
@@ -7,6 +7,7 @@ from matplotlib.image import imread
 import itertools
 import random
 
+
 def image_visualizer_body():
     """
     Display visualization options of cherry leaf images:
@@ -14,50 +15,86 @@ def image_visualizer_body():
     * Difference between averages images
     * Image Montage
     """
-    
+
     st.write("### Cherry Leaf Image Visualizer")
 
-    st.info("* This tool helps explore cherry leaf images and visualize patterns related to powdery mildew.")
-    
+    st.info(
+        "* This tool helps explore cherry leaf images and visualize"
+        "patterns related to powdery mildew."
+        )
+
     version = 'v1'
     if st.checkbox("Average and Variability Images"):
-        
-      avg_infected = plt.imread(f"outputs/{version}/avg_var_powdery_mildew.png")
-      avg_healthy = plt.imread(f"outputs/{version}/avg_var_healthy.png")
 
-      st.image(avg_infected, caption='Infected Leaf - Average and Variability')
-      st.image(avg_healthy, caption='Healthy Leaf - Average and Variability')
+        avg_infected = plt.imread(
+            f"outputs/{version}/avg_var_powdery_mildew.png"
+            )
+        avg_healthy = plt.imread(f"outputs/{version}/avg_var_healthy.png")
 
-      st.write("These images show the average image of powdery mildew infected and healthy leaves. Differences in aspects like color or shape help the model to distinguish between the two classes.")
-      
-      st.write("---")
-    
+        st.image(
+            avg_infected,
+            caption='Infected Leaf - Average and Variability'
+            )
+        st.image(avg_healthy, caption='Healthy Leaf - Average and Variability')
+
+        st.write(
+            "These images show the average image of powdery mildew infected"
+            "and healthy leaves. Differences in aspects like color or shape"
+            "help the model to distinguish between the two classes."
+            )
+
+        st.write("---")
+
     if st.checkbox("Difference Between Average Images"):
-      diff_between_avgs = plt.imread(f"outputs/{version}/avg_diff.png")
-      st.image(diff_between_avgs, caption='Difference between Average Images')
+        diff_between_avgs = plt.imread(f"outputs/{version}/avg_diff.png")
+        st.image(
+            diff_between_avgs,
+            caption='Difference between Average Images'
+            )
 
-      st.write("Here we can see how the average image of a healthy leaf and average image of an infected leaf differ on average. This is helpful for feature extraction.")
+        st.write(
+            "Here we can see how the average image of a healthy leaf"
+            "and average image of an infected leaf differ on average."
+            "This is helpful for feature extraction."
+            )
 
     if st.checkbox("Image Montage"):
-      st.write("* To refresh the montage, click on the 'Create Montage' button")
-      my_data_dir = 'inputs/mildew_dataset/cherry-leaves'
+        st.write(
+                "* To refresh the montage, click on the"
+                "'Create Montage' button"
+                )
+        my_data_dir = 'inputs/mildew_dataset/cherry-leaves'
 
-      # Get only real directories in validation folder and exclude hidden system files
-      validation_dir = os.path.join(my_data_dir, "validation")
-      labels = [d for d in os.listdir(validation_dir) if os.path.isdir(os.path.join(validation_dir, d))]
-        
-      label_to_display = st.selectbox("Select Label", options=labels)
+        # Get only real directories in validation folder
+        # exclude hidden system files
+        validation_dir = os.path.join(my_data_dir, "validation")
+        labels = [
+            d
+            for d in os.listdir(validation_dir)
+            if os.path.isdir(os.path.join(validation_dir, d))
+            ]
 
-      if st.button("Create Montage"):
+        label_to_display = st.selectbox("Select Label", options=labels)
 
-        st.write(f"This montage shows us the diversity of images of one category in the dataset.")
+        if st.button("Create Montage"):
 
-        image_montage(dir_path=os.path.join(my_data_dir, "validation"), label_to_display=label_to_display, nrows=8, ncols=3, figsize=(10,25))
+            st.write(
+                "This montage shows us the diversity of images of"
+                "one category in the dataset."
+                )
 
-      st.write("---")
+            image_montage(
+                dir_path=os.path.join(my_data_dir, "validation"),
+                label_to_display=label_to_display,
+                nrows=8,
+                ncols=3,
+                figsize=(10, 25)
+                )
+
+        st.write("---")
 
 
-def image_montage(dir_path, label_to_display, nrows, ncols, figsize=(15,10)):
+def image_montage(dir_path, label_to_display, nrows, ncols, figsize=(15, 10)):
     """
     Generate and display a montage of images from validation image dataset
     """
@@ -66,8 +103,9 @@ def image_montage(dir_path, label_to_display, nrows, ncols, figsize=(15,10)):
 
     # Filter images per label
     if label_to_display in labels:
-        
-        # Get list of images in selected folder, check if montage space > subset size
+
+        # Get list of images in selected folder
+        # check if montage space > subset size
         images_list = os.listdir(os.path.join(dir_path, label_to_display))
 
         # Check if montage size is valid
@@ -82,8 +120,8 @@ def image_montage(dir_path, label_to_display, nrows, ncols, figsize=(15,10)):
             return
 
         # Create list of axes indices based on nrows and ncols
-        list_rows = range(0,nrows)
-        list_cols = range(0,ncols)
+        list_rows = range(0, nrows)
+        list_cols = range(0, ncols)
         plot_idx = list(itertools.product(list_rows, list_cols))
 
         # Create a figure and display images
@@ -92,11 +130,13 @@ def image_montage(dir_path, label_to_display, nrows, ncols, figsize=(15,10)):
             img = imread(os.path.join(dir_path, label_to_display, img_idx[x]))
             img_shape = img.shape
             axes[plot_idx[x][0], plot_idx[x][1]].imshow(img)
-            axes[plot_idx[x][0], plot_idx[x][1]].set_title(f"Width {img_shape[1]}px x Height {img_shape[0]}px")
+            axes[plot_idx[x][0], plot_idx[x][1]].set_title(
+                "Width {img_shape[1]}px x Height {img_shape[0]}px"
+                )
             axes[plot_idx[x][0], plot_idx[x][1]].set_xticks([])
             axes[plot_idx[x][0], plot_idx[x][1]].set_yticks([])
         plt.tight_layout()
-        
+
         st.pyplot(fig=fig)
 
     else:
