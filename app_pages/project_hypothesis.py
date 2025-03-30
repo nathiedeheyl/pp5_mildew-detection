cc26267bd0e3916fb0124dc04ed8630f09d7c482
nathiedeheyl <n.vonheyl@outlook.com>
2025-03-30 18:55:54 +0200

    fix: syntax errors, apply PEP8 formatting

--- app_pages/project_hypothesis.py
+++ app_pages/project_hypothesis.py
@@ -8,10 +8,11 @@ def project_hypothesis_body():
     st.write("### Project Hypothesis and Validation")
 
     st.success(
-        f"* Hypothesis: Cherry leaves affected by powdery mildew exhibit distinct visual patterns, "
-        f"e.g. white powdery spots, that clearly differentiate them from healthy leaves.\n\n"
-        f"* Validation: The Image Montage (see 'Image Visualizer') reveals that leaves infected "
-        f"with powdery mildew often display visible white patches. "
-        f"However, the difference between average image of both classes analysis "
-        f"does not clearly highlight distinct patterns to differentiate."
+        "**Hypothesis:** Cherry leaves affected by powdery mildew show distinctive visual characteristics "
+        "compared to healthy leaves, making it possible to differentiate them through image analysis. "
+        "Infected leaves are expected to display light, irregular spots, in contrast to the general green color of healthy leaves.\n\n"
+        "**Validation:** To test this hypothesis, average images and variability images were generated for each class ('healthy' and 'powdery mildew'). "
+        "These visualizations confirm that infected leaves typically reveal a dotted pattern not present in healthy leaves, supporting the hypothesis. "
+        "Additionally, the ML model trained on this dataset achieves a high predictive accuracy, further validating that these differences "
+        "are significant enough for automated classification."
         )
