9d4a16802a56fb938f72d6f97430b18b12887234
nathiedeheyl <n.vonheyl@outlook.com>
2025-03-30 15:46:44 +0200

    fix: syntax errors, PEP8 formatting, remove unnecessary matplotlib imread function import

--- app_pages/ml_performance_metrics.py
+++ app_pages/ml_performance_metrics.py
@@ -1,12 +1,14 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 import pandas as pd
-from matplotlib.image import imread
 from src.machine_learning.evaluate_clf import load_test_evaluation
+
 
 def ml_performance_metrics():
     """
-    Display model performance metrics: class label distribution, training history, test dataset evaluation, and business requirement conclusions
+    Display model performance metrics: class label distribution,
+    training history, test dataset evaluation,
+    and business requirement conclusions
     """
     version = 'v1'
 
@@ -16,15 +18,20 @@ def ml_performance_metrics():
     # Label distribution
     st.write("#### 1. Dataset Composition")
     st.write("About the distribution of image labels in the dataset:")
-    labels_distribution = plt.imread(f"outputs/{version}/labels_distribution.png")
-    st.image(labels_distribution, caption='Labels Distribution on Train, Validation and Test Sets')
-    
+    labels_distribution = plt.imread(
+                                f"outputs/{version}/labels_distribution.png"
+                                )
+    st.image(
+        labels_distribution,
+        caption='Labels Distribution on Train, Validation and Test Sets'
+        )
+
     st.write("---")
 
     # Model training history
     st.write("#### 2. Model Training Performance")
     col1, col2 = st.columns(2)
-    with col1: 
+    with col1:
         model_acc = plt.imread(f"outputs/{version}/model_training_acc.png")
         st.image(model_acc, caption='Model Training Accuracy')
     with col2:
@@ -40,6 +47,31 @@ def ml_performance_metrics():
     # Business conclusions Section
     st.write("### 4. Business Insights and Conclusions")
 
+    st.write(
+        "Conclusions of data analytics task to answer business requirements:"
+        )
+    st.write(
+        "The aim of this study was to help the client in their goal of "
+        "visually differentiating healthy cherry leaves from "
+        "powdery mildew-infected leaves and developing an early "
+        "disease prediction model."
+        )
+
+    st.write(
+        "While the client's original requirement was a binary "
+        "classification system, the model is based on a multiclass "
+        "model for more differentiation. The multiclass approach "
+        "allows for a detailed prediction number per class instead of "
+        "deciding for one class directly and communicating it to the "
+        "customer. This allows the client to make an informed "
+        "decision based on the probability of "
+        "infection of the crop."
+        )
+
     # TODO: If possible, dynamically extract accuracy from 'test_eval'
-    st.write("Conclusions of data analytics task to answer business requirements:")
-    st.write(f"The model has a predictive accuracy of 99%, indicating that it meets business requirement #3.")
+    st.write(
+        "The mildew detection model at hand achieved an **accuracy of 99%** "
+        "This guarantees prediction reliability, preventing the client "
+        "from experiencing supply chain interruptions or other obstacles "
+        "by avoiding compromised-quality produce."
+        )
